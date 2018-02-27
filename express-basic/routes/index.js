f9dd6715444404f3a64424f5144b9c3f42791921
Josef Pospíšil <josef.pospisil@laststar.eu>
2018-02-27 13:41:41 +0100

    Tweak basic node application

--- express-basic/routes/index.js
+++ express-basic/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CULS CBD - Basic Express application' });
+  res.render('index', { title: 'CULS CBD - Basic Express application in the year of the Dog' });
 });
 
 module.exports = router;
--- express-basic/routes/users.js
+++ express-basic/routes/users.js
@@ -6,12 +6,12 @@ var users = [{id: 1, name: "Josef", account: "pepe"},
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send(users);
+  res.send(users);
 });
 
 router.get('/:id', function(req, res, next) {
-    var user = users.filter(function(u) {return req.params.id == u.id})[0];
-    res.send(user);
+  var user = users.filter(function(u) {return req.params.id == u.id;})[0];
+  res.send(user);
 });
 
 module.exports = router;
