cfc83c2eb169cb962a1b5d4ef20f1c98031b4f5a
Josef Pospíšil <josef.pospisil@laststar.eu>
2018-02-28 18:53:10 +0100

    Add user Schema with validators

--- express-mongoose/routes/users.js
+++ express-mongoose/routes/users.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost/showrumbackend');
 
-const User = mongoose.model('User',
-                          {
-                            login: String,
-                            password: String,
-                            name: String,
-                            email: String
-                          });
+const userSchema =
+      new Schema({
+        login: {type: String, required: true, minlength: 4},
+        password: {type: String, required: true},
+        name: {type: String, required: true},
+        email: {type: String, required: true}
+      });
+const User = mongoose.model('User', userSchema);
 
 router.post('/', function(req, res, next) {
   const body = req.body;
@@ -22,17 +24,18 @@ router.post('/', function(req, res, next) {
       email: body.email
     });
 
-    user.save(function (err) {
-      if (err) {
-        console.error(err);
-        res.status(422);
-        res.send();
-      } else {
+    user.save()
+      .then((user) => {
         res.status(201);
         res.location("/users/" + user.id);
         res.send();
-      }
-    });
+
+      })
+      .catch((err) =>  {
+        console.error(err);
+        res.status(422);
+        res.send();
+      });
   } else {
 
     res.status(422);
