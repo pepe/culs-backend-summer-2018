1792124c3194678096030a952d0285b96557b634
Josef Pospíšil <josef.pospisil@laststar.eu>
2018-02-28 14:45:30 +0100

    Upgrade and format

--- express-mongoose/routes/users.js
+++ express-mongoose/routes/users.js
@@ -6,64 +6,66 @@ mongoose.connect('mongodb://localhost/showrumbackend');
 
 var User = mongoose.model('User',
                           {
-                              login: String,
-                              password: String,
-                              name: String,
-                              email: String
+                            login: String,
+                            password: String,
+                            name: String,
+                            email: String
                           });
 
 router.post('/', function(req, res, next) {
-    body = req.body;
-    console.log(body);
-    if (body.password == body.confirmation) {
-        user = new User({
-            login: body.login,
-            password: body.password,
-            name: body.name,
-            email: body.email
-        });
+  console.log("Creating user");
+  var body = req.body;
+  console.log(body);
+  if (body.password == body.confirmation) {
+    var user = new User({
+      login: body.login,
+      password: body.password,
+      name: body.name,
+      email: body.email
+    });
 
-        user.save(function (err) {
-            if (err) {
-                console.log(err);
-                res.status(422);
-                res.send();
-            } else {
-                res.status(201);
-                res.location("/users/" + user.id);
-                res.send();
-            }
-        });
-    } else {
+    user.save(function (err) {
+      if (err) {
+        console.log(err);
         res.status(422);
         res.send();
-    }
+      } else {
+        res.status(201);
+        res.location("/users/" + user.id);
+        res.send();
+      }
+    });
+  } else {
+
+    res.status(422);
+    res.send();
+  }
 });
 
 router.post('/authenticate', function(req, res, next) {
-    body = req.body;
-    console.log(body);
-    var user = User.findOne({login: body.login, password: body.password},
-                            "_id login",
-                            function(err, user) {
-                                if (err) {
-                                    console.log(err);
-                                    res.status(401);
-                                    res.send();
-                                } else {
-                                    console.log(user)
-                                    if (user == null)
-                                    {
-                                        console.log(err);
-                                        res.status(401);
-                                        res.send();
-                                    } else {
-                                        res.status(200);
-                                        res.send({login: user.login,
-                                                  token: user.id});
-                                    }
-                                }
-                            });
+  body = req.body;
+  console.log(body);
+  var user = User.findOne({login: body.login, password: body.password},
+                          "_id login",
+                          function(err, user) {
+                            if (err) {
+                              console.log(err);
+                              res.status(401);
+                              res.send();
+                            } else {
+                              console.log(user)
+                              if (user == null)
+                              {
+                                console.log(err);
+                                res.status(401);
+                                res.send();
+                              } else {
+                                res.status(200);
+                                res.send({login: user.login,
+                                          token: user.id});
+                              }
+                            }
+                          });
 });
 
 module.exports = router;
